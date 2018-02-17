40a0a2c4e209960478cd617d89cada9504eb93bd
ellenli030 <31971053+ellenli030@users.noreply.github.com>
2018-02-17 17:56:54 -0500

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -14,3 +14,25 @@ public class TicTacToe {
 	Scanner scanner = new Scanner(System.in);
 	System.out.println("");
 	user2 = scanner.nextInt();
+	
+	public String userInput() {
+		String input;
+		System.out.println("Enter a coordinate: ");
+		input = scanner.nextLine(); //potentially fix
+
+		String temp = input[0];
+
+		int row;
+		if (temp == 'A') {
+			row = 0;
+		}
+		else if (temp == 'B') {
+			row = 1;
+		}
+		else if (temp == 'C') {
+			row = 2;
+		}
+		else {
+			System.out.print("Please try again");
+			userInput();
+		}
