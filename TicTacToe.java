d656394e0bf54137c8bdf49d99f9eafc16f94b15
viviandiec <viviandiec@gmail.com>
2018-02-17 17:50:03 -0500

    Create TicTacToe.java

---
+++ TicTacToe.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TicTacToe {
+
+	String board[3][3];
+	int user1, user2;
+	public int try;
+
+	Scanner scanner = new Scanner(System.in);
+	System.out.println("");
+	user1 = scanner.nextInt();
+
+	Scanner scanner = new Scanner(System.in);
+	System.out.println("");
+	user2 = scanner.nextInt();
