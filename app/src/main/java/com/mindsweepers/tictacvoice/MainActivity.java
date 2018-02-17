e4e62a3609d688347859da3da5ae771a4e4fdaa2
Michael Lumibao <31720028+Michaellumibao@users.noreply.github.com>
2018-02-17 18:11:08 -0500

    Added Speech Playback Functionality

--- app/src/main/java/com/mindsweepers/tictacvoice/MainActivity.java
+++ app/src/main/java/com/mindsweepers/tictacvoice/MainActivity.java
@@ -2,8 +2,10 @@ package com.mindsweepers.tictacvoice;
 
 import android.content.Intent;
 import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,6 +13,7 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Locale;
+import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +21,10 @@ public class MainActivity extends AppCompatActivity {
     private Button input_btn;
     private TextView textMessage;
 
+    private int result;
     private String userInput;
+
+    private TextToSpeech toSpeech;
 
 
     @Override
@@ -27,7 +33,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textMessage = (TextView) findViewById(R.id.textMessage);
+        toSpeech = new TextToSpeech(MainActivity.this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status){
+                if (status == TextToSpeech.SUCCESS){
+                    result = toSpeech.setLanguage(Locale.UK);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Feature not supported in your device", Toast.LENGTH_SHORT).show();
+                }
 
+            }
+        });
+
+    }
+
+    public void playbackOutput(View view){
+        Log.i("playbackOutput", "PlayBack OutPut");
+        switch (view.getId()){
+            case R.id.playback_btn:
+                Log.i("userInput","In text message case");
+
+                if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED){
+                    Toast.makeText(getApplicationContext(), "Feature not supported in your device", Toast.LENGTH_SHORT).show();
+                } else {
+                    userInput = textMessage.getText().toString();
+                    Log.i("userInput",userInput);
+                    toSpeech.speak(userInput,TextToSpeech.QUEUE_FLUSH,null);
+                }
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (toSpeech != null){
+            toSpeech.stop();
+            toSpeech.shutdown();
+        }
     }
 
     public void getSpeechInput(View view){
